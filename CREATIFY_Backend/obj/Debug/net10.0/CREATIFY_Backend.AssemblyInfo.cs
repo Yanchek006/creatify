70bfb71881e8aad08868b8a03f2e6cc3f7b85344
Yanchek006 <yana-006@mail.ru>
2026-04-11 22:23:45 +0300

    pages add

--- CREATIFY_Backend/obj/Debug/net10.0/CREATIFY_Backend.AssemblyInfo.cs
+++ CREATIFY_Backend/obj/Debug/net10.0/CREATIFY_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3551a912d167074f044d6ba0cac993e53ff501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed847e0e10a3fda7d312855e98edea0c759c1cb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
