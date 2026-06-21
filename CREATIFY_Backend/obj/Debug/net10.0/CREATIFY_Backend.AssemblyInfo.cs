016fad14c8567766ebea893665d02a2a8c2de7e5
Yanchek006 <yana-006@mail.ru>
2026-06-21 20:56:34 +0300

    plinks

--- CREATIFY_Backend/obj/Debug/net10.0/CREATIFY_Backend.AssemblyInfo.cs
+++ CREATIFY_Backend/obj/Debug/net10.0/CREATIFY_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70bfb71881e8aad08868b8a03f2e6cc3f7b85344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3934094e794e9c3b06d52f1d04514ab48a859d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
