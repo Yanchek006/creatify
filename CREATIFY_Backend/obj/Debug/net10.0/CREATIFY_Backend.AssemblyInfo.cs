942e570ad6477da2724c082facd3fda561948c3b
Yanchek006 <yana-006@mail.ru>
2026-07-13 00:50:08 +0300

    update pic infogr

--- CREATIFY_Backend/obj/Debug/net10.0/CREATIFY_Backend.AssemblyInfo.cs
+++ CREATIFY_Backend/obj/Debug/net10.0/CREATIFY_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d53c40179a8173650992c4079052f6190fb3f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb434830e51bc01d91fefaeec457768c85e39e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CREATIFY_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
